b02c1b8d98e5f0edcb139767db07127ca794a151
PauloMatheusSenac <paulo54098136@aluno.ms.senac.br>
2024-09-26 07:39:36 -0400

    Update BD BancoParca.sql

--- BD BancoParca.sql
+++ BD BancoParca.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE bancoparca;
 USE bancoparca;
 
-
 CREATE TABLE perfis (
     id INT AUTO_INCREMENT PRIMARY KEY,
     tipo_perfil ENUM('Gerente Geral', 'Chefe de Setor', 'Funcionário', 'Cliente') NOT NULL,
@@ -10,19 +9,17 @@ CREATE TABLE perfis (
     celular VARCHAR(20),
     sobrenome VARCHAR(50),
     email VARCHAR(100),
-    cpf VARCHAR(11) UNIQUE
+    cpf CHAR(11) UNIQUE NOT NULL
 );
 
 CREATE TABLE contas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titular VARCHAR(100) NOT NULL UNIQUE,
     saldo DECIMAL(10, 2) NOT NULL DEFAULT 0,
-    cpf VARCHAR(11),
-    numero_conta VARCHAR(20),
-    FOREIGN KEY (cpf) REFERENCES perfis(cpf) 
+    cpf CHAR(11),
+    numero_conta VARCHAR(20) UNIQUE,
+    FOREIGN KEY (cpf) REFERENCES perfis(cpf)
 );
-
-
 
 INSERT INTO perfis (tipo_perfil, titular, senha, celular, sobrenome, email, cpf) VALUES 
 ('Gerente Geral', 'admin', 'senha_admin', '1234567890', 'Admin', 'admin@example.com', '12345678910'),
